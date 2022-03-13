5b5a526dc47d927202e3171faf6ac807bfa68c1a
Chin <greeting.to.chin@gmail.com>
2022-03-13 12:58:23 +0800

    chore(unocss): setup font and basic style

--- unocss.config.js
+++ unocss.config.js
@@ -1,6 +1,30 @@
-import presetUno from '@unocss/preset-uno';
 import { defineConfig } from 'unocss';
+import presetUno from '@unocss/preset-uno';
+import presetAttributify from '@unocss/preset-attributify';
+import presetWebFonts from '@unocss/preset-web-fonts';
+import transformerDirective from '@unocss/transformer-directives';
 
 export default defineConfig({
-  presets: [presetUno()]
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetWebFonts({
+      provider: 'google',
+      fonts: {
+        sans: 'Noto+Sans+TC'
+      }
+    })
+  ],
+  theme: {
+    colors: {
+      gray: {
+        50: '#F8F8F8'
+      }
+    },
+    breakpoints: {
+      xs: '360px',
+      sm: '768px'
+    }
+  },
+  transformers: [transformerDirective()]
 });
