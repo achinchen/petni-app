2516445f39e42ff94d30a0e94d90b7612895785d
Chin <greeting.to.chin@gmail.com>
2022-03-13 14:57:49 +0800

    feat(components/logo): add type to control different asset source

---
+++ app/components/logo/constants.ts
@@ -0,0 +1,9 @@
+import VerticalLogo from '~/images/logo/vertical.gif';
+import HorizontalLogo from '~/images/logo/horizontal.png';
+
+export const SOURCE = {
+  horizontal: HorizontalLogo,
+  vertical: VerticalLogo
+};
+
+export { APP_NAME } from '~/constants';
