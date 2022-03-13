597ee8bd1faff16c3af4b1bcb9eed968df91552a
Chin <greeting.to.chin@gmail.com>
2022-03-13 11:41:42 +0800

    chore(unocss): remove unocss-preset-attributify

--- unocss.config.js
+++ unocss.config.js
@@ -1,10 +1,6 @@
-import presetAttributify from '@unocss/preset-attributify';
 import presetUno from '@unocss/preset-uno';
 import { defineConfig } from 'unocss';
 
 export default defineConfig({
-  presets: [presetUno(), presetAttributify()],
-  extendTheme: {
-    // #F8F8F8
-  }
+  presets: [presetUno()]
 });
