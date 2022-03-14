6e5fc0c2e591828b36a26ef33c651c9c23e5943e
Chin <greeting.to.chin@gmail.com>
2022-03-14 21:34:54 +0800

    feat(features/theme/color): prepare theme-color context

---
+++ app/features/theme/color/constants/cats.ts
@@ -0,0 +1,56 @@
+import OrangeCat from '~/images/theme/cat/orange.png';
+import CalicoCat from '~/images/theme/cat/calico.png';
+import WhiteCat from '~/images/theme/cat/white.png';
+import BlackCat from '~/images/theme/cat/black.png';
+import TuxedoCat from '~/images/theme/cat/tuxedo.png';
+import TabbyCat from '~/images/theme/cat/tabby.png';
+import TortoiseshellCat from '~/images/theme/cat/tortoiseshell.png';
+
+export const CATS = [
+  {
+    key: 'orange',
+    label: '橘貓',
+    characteristic: '個性膽小、貪吃、愛講話，十隻橘貓九隻胖。',
+    image: OrangeCat
+  },
+  {
+    key: 'calico',
+    label: '三花貓',
+    characteristic: '個性黏人、愛撒嬌，因為遺傳基因的關係，99%以上都是母貓。',
+    image: CalicoCat
+  },
+  {
+    key: 'white',
+    label: '白貓',
+    characteristic:
+      '優雅乖巧、個性獨立。撒嬌時，會輕聲喵喵叫，或是輕拍主人討摸摸。',
+    image: WhiteCat
+  },
+  {
+    key: 'black',
+    label: '黑貓',
+    characteristic:
+      '反差萌、愛撒嬌、聰明、貼心、學習力強、會認人，對牠好，牠會記在心裡。',
+    image: BlackCat
+  },
+  {
+    key: 'tuxedo',
+    label: '乳牛貓',
+    characteristic: '貓界的哈士奇、個性好動，容易跟其他貓打成一片。',
+    image: TuxedoCat
+  },
+  {
+    key: 'tabby',
+    label: '虎斑貓',
+    characteristic:
+      '野性美、狩獵高手、傲嬌脾氣、警戒心強，一旦被牠認定，就會很依賴主子。',
+    image: TabbyCat
+  },
+  {
+    key: 'tortoiseshell',
+    label: '玳瑁貓',
+    characteristic:
+      '個性善良、脾氣最好、愛撒嬌、不挑食，99%以上都是母貓，親人也親貓。',
+    image: TortoiseshellCat
+  }
+];
---
+++ app/features/theme/color/constants/dogs.ts
@@ -0,0 +1,59 @@
+import CalicoDog from '~/images/theme/dog/calico.png';
+import RustDog from '~/images/theme/dog/rust.png';
+import GoldDog from '~/images/theme/dog/gold.png';
+import WhiteDog from '~/images/theme/dog/white.png';
+import BlackDog from '~/images/theme/dog/black.png';
+import TwoColorDog from '~/images/theme/dog/two-color.png';
+import TabbyDog from '~/images/theme/dog/tabby.png';
+import GrayDog from '~/images/theme/dog/gray.png';
+
+export const DOGS = [
+  {
+    key: 'calico',
+    label: '三色狗',
+    characteristic: '個性老實、喜歡社交，溫順黏人的小跟屁蟲。',
+    image: CalicoDog
+  },
+  {
+    key: 'rust',
+    label: '紅棕狗',
+    characteristic: '個性獨立、外向、喜歡與人互動。',
+    image: RustDog
+  },
+  {
+    key: 'gold',
+    label: '黃狗',
+    characteristic: '溫和友善、忠誠護主、乖巧聽話、不太主動攻擊人。',
+    image: GoldDog
+  },
+  {
+    key: 'white',
+    label: '白狗',
+    characteristic: '憂雅氣質、乖巧安靜。白毛容易髒，需要花心思照顧。',
+    image: WhiteDog
+  },
+  {
+    key: 'black',
+    label: '黑狗',
+    characteristic: '個性穩重、敏捷聰明、學習力強。',
+    image: BlackDog
+  },
+  {
+    key: 'two-color',
+    label: '雙色狗',
+    characteristic: '活潑好動、憨厚、愛玩、天生少根筋、無厘頭，親人也親狗。',
+    image: TwoColorDog
+  },
+  {
+    key: 'tabby',
+    label: '虎斑狗',
+    characteristic: ' 身上有著老虎斑點，面惡心善、忠心勇敢、聰明機警。',
+    image: TabbyDog
+  },
+  {
+    key: 'gray',
+    label: '灰狗',
+    characteristic: '善良優雅、好奇心重、喜歡嘗試新鮮事物、樂於取悅主人。',
+    image: GrayDog
+  }
+];
