ca217ed1d1caca6321e18d604bfdee219ea48ce0
Chin <greeting.to.chin@gmail.com>
2022-03-28 22:25:34 +0800

    feat(components/credit-loading): Add credit loading

---
+++ app/components/credit-loading/constants.ts
@@ -0,0 +1,5 @@
+import { APP_NAME } from '~/constants';
+
+const PROGRAMMER = 'Chin Chen';
+const DESIGNER = 'K.T';
+export const CREDITS = `${APP_NAME} © Code: ${PROGRAMMER}  /  Design: ${DESIGNER}`;
