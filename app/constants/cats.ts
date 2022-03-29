29f74853250740c4f09091086a52a3239cf50c2c
Chin <greeting.to.chin@gmail.com>
2022-03-29 22:10:27 +0800

    refactor(features/theme/color/constants): use enum DogColor and CatColor to manipulate constants

---
+++ app/constants/cats.ts
@@ -0,0 +1,19 @@
+export enum CatColor {
+  Orange = 'ORANGE',
+  Calico = 'CALICO',
+  White = 'WHITE',
+  Black = 'BLACK',
+  Tuxedo = 'TUXEDO',
+  Tabby = 'TABBY',
+  Tortoiseshell = 'TORTOISESHELL'
+}
+
+export const CAT_COLOR_LABEL = {
+  [CatColor.Orange]: '橘貓',
+  [CatColor.Calico]: '三花貓',
+  [CatColor.White]: '白貓',
+  [CatColor.Black]: '黑貓',
+  [CatColor.Tuxedo]: '乳牛貓',
+  [CatColor.Tabby]: '虎斑貓',
+  [CatColor.Tortoiseshell]: '玳瑁貓'
+};
---
+++ app/constants/dogs.ts
@@ -0,0 +1,21 @@
+export enum DogColor {
+  Calico = 'CALICO',
+  Rust = 'RUST',
+  Gold = 'GOLD',
+  White = 'WHITE',
+  Black = 'BLACK',
+  TwoColor = 'TWO_COLOR',
+  Tabby = 'TABBY',
+  Gray = 'GRAY'
+}
+
+export const DOG_COLOR_LABEL = {
+  [DogColor.Calico]: '三色狗',
+  [DogColor.Rust]: '紅棕狗',
+  [DogColor.Gold]: '黃狗',
+  [DogColor.White]: '白狗',
+  [DogColor.Black]: '黑狗',
+  [DogColor.TwoColor]: '雙色狗',
+  [DogColor.Tabby]: '虎斑狗',
+  [DogColor.Gray]: '灰狗'
+};
