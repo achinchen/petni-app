6bf3c3e73877cebfd92825533c96c308d5e9e8d0
Chin <greeting.to.chin@gmail.com>
2022-04-10 22:10:45 +0800

    feat(IconButton): add onClick

--- app/components/IconButton/index.tsx
+++ app/components/IconButton/index.tsx
@@ -4,15 +4,17 @@ import Icon, { IconType } from '~/components/icon';
 type Props = {
   icon: IconType;
   iconAttributifyOptions: AttributifyOptions;
+  onClick: () => void;
 } & AttributifyOptions;
 
 export default function IconButton({
   icon,
+  onClick,
   iconAttributifyOptions,
   ...attributifyOptions
 }: Props) {
   return (
-    <button bg="white" {...attributifyOptions}>
+    <button bg="white" {...attributifyOptions} onClick={onClick}>
       <Icon icon={icon} {...iconAttributifyOptions} />
     </button>
   );
