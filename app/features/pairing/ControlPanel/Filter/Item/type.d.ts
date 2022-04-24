0b10809ed195f6cd15499442357530e196840e47
Chin <greeting.to.chin@gmail.com>
2022-04-24 11:15:16 +0800

    fix(pairing/type): fix path and remove unused constants

--- app/features/pairing/ControlPanel/Filter/Item/type.d.ts
+++ app/features/pairing/ControlPanel/Filter/Item/type.d.ts
@@ -1,18 +1,14 @@
 import type {
-  Family,
   FamilyOptions,
-  Gender,
   GenderOptions,
-  Age,
   AgeOptions,
-  Color,
   ColorOptions
-} from '~/features/pairingPet/ControlPanel/Filter/type';
+} from '~/features/pairing/ControlPanel/Filter/type';
 
 import {
   GENERAL_FILTER_OPTIONS,
   DYNAMIC_FILTER_OPTIONS
-} from '~/features/pairingPet/ControlPanel/Filter/constants';
+} from '~/features/pairing/ControlPanel/Filter/constants';
 
 export type Category =
   | typeof GENERAL_FILTER_OPTIONS[number]['CATEGORY']
