058c511603909b023ae1d04d5b6e4ede4bb3ff2a
Chin <greeting.to.chin@gmail.com>
2022-05-01 22:32:54 +0800

    chore(test): remove unused file

--- test/index.ts
+++
@@ -1,9 +0,0 @@
-import path from 'path';
-
-export default {
-  process(sourceText: string, sourcePath: string) {
-    return {
-      code: `module.exports = ${JSON.stringify(path.basename(sourcePath))};`
-    };
-  }
-};
