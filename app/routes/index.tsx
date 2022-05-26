c54c724b3796583fe3d7fc94151690f35aef62f4
Chin <greeting.to.chin@gmail.com>
2022-05-26 23:20:05 +0800

    chore(routes/index): use default meta

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,5 @@
 import type { Animal } from '@prisma/client';
-import type { LoaderFunction, MetaFunction } from '@remix-run/node';
+import type { LoaderFunction } from '@remix-run/node';
 import { useEffect, useState } from 'react';
 import PetPairing from '~/features/pairing';
 import FullPageLoading from '~/components/common/FullPageLoading';
@@ -7,15 +7,6 @@ import { useLoaderData } from '@remix-run/react';
 import { json } from '@remix-run/node';
 import Layout from '~/components/common/Layout';
 import { db } from '~/utils/db.server';
-import { DEFAULT_META } from '~/constants/meta';
-
-export const meta: MetaFunction = ({
-  data
-}: {
-  data: LoaderData | undefined;
-}) => {
-  return DEFAULT_META;
-};
 
 type LoaderData = { animals: Animal[] };
 
