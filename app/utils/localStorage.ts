e01725a0c8245d68dca73d491617ae3c79453154
Chin <greeting.to.chin@gmail.com>
2022-06-03 16:45:16 +0800

    fix(utils/localStorage): fix getLocalStorage with null

--- app/utils/localStorage.ts
+++ app/utils/localStorage.ts
@@ -1,5 +1,7 @@
-export const getLocalStorage = (key: string) =>
-  JSON.stringify(localStorage.getItem(key));
+export const getLocalStorage = (key: string) => {
+  const value = localStorage.getItem(key);
+  return value ? JSON.parse(value) : '';
+};
 
 export const setLocalStorage = (key: string, payload: any) =>
   localStorage.setItem(key, JSON.stringify(payload));
