cca798f886412dc84b1178126059a453806c7372
Chin <greeting.to.chin@gmail.com>
2022-06-03 16:46:32 +0800

    feat(constants): make Family and Gender type consistent

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,16 +1,3 @@
+export { Family, Gender, Size } from '@prisma/client';
+
 export const APP_NAME = 'PetNi';
-
-export enum Family {
-  Cat = 'CAT',
-  Dog = 'DOG'
-}
-
-export enum Gender {
-  Male = 'Male',
-  Female = 'Female'
-}
-
-export enum Age {
-  Kid = 'KID',
-  Adult = 'ADULT'
-}
