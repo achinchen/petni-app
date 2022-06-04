760b73c5c2df170f2e93394841d08e16ac584c5f
Chin <greeting.to.chin@gmail.com>
2022-06-04 16:49:23 +0800

    chore(pairing/ControlPanel/utils): tidy up filter preference

---
+++ app/features/pairing/ControlPanel/utils/index.ts
@@ -0,0 +1,7 @@
+import type { FilterState } from '~/features/pairing/ControlPanel/hooks/useFilterState';
+import { getLocalStorage, setLocalStorage } from '~/utils/storage';
+
+const PREFERENCE_KEY = 'Filter';
+export const getFilterPreference = () => getLocalStorage(PREFERENCE_KEY);
+export const setFilterPreference = (payload: FilterState) =>
+  setLocalStorage(PREFERENCE_KEY, JSON.stringify(payload));
