e6d5e3b6ecbc483578899e78afd6935b55cc87c8
Chin <greeting.to.chin@gmail.com>
2022-06-05 21:57:18 +0800

    feat(pairing/ControlPanel/Filter): reset filter.color when family be changed

--- app/features/pairing/ControlPanel/Filter/index.tsx
+++ app/features/pairing/ControlPanel/Filter/index.tsx
@@ -2,9 +2,12 @@ import type { Family, Gender, Size, Color } from './type';
 import FilterItem from '~/features/pairing/ControlPanel/Filter/Item';
 import { useControlContext } from '~/features/pairing/ControlPanel/context';
 import {
+  DEFAULT_OPTION,
+  FAMILY_OPTION,
   GENERAL_FILTER_OPTIONS,
   COLOR_OPTION
 } from '~/features/pairing/ControlPanel/constants/filter';
+import { useEffect } from 'react';
 
 type Filter = {
   family: Family;
@@ -21,8 +24,16 @@ export default function FilterPanel() {
 
   const showColorFilter = Boolean(COLOR_OPTION.OPTIONS(filter.family).length);
 
+  const resetColor = () => {
+    dispatchFilter({
+      type: COLOR_OPTION.CATEGORY,
+      value: DEFAULT_OPTION.VALUE
+    });
+  };
+
   const onFilterClick = (type: FilterType) => (value: Payload) => () => {
     dispatchFilter({ type, value });
+    if (type === FAMILY_OPTION.CATEGORY) resetColor();
   };
 
   const getIsPressed = (type: FilterType) => (value: Payload) => {
