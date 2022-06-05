2ba1799091b91a2bc792724188fd81fa4d5133ed
Chin <greeting.to.chin@gmail.com>
2022-06-05 13:45:31 +0800

    refactor(ControlPanel/FilterItem): tidy up style

--- app/features/pairing/ControlPanel/Filter/Item/index.tsx
+++ app/features/pairing/ControlPanel/Filter/Item/index.tsx
@@ -1,6 +1,7 @@
 import type { Category, Label, Options } from './type';
 import Icon from '~/components/common/Icon';
 import OptionButton from './OptionButton';
+import { BUTTON_SIZE } from './constants';
 
 type Props = {
   category: Category;
@@ -10,6 +11,8 @@ type Props = {
   onClick: (value: any) => () => void;
 };
 
+const SMALL_BUTTON_CATEGORIES = ['size', 'color'];
+
 export default function Filter({
   category,
   label,
@@ -17,19 +20,21 @@ export default function Filter({
   getIsPressed,
   onClick
 }: Props) {
-  const buttonSize = ['size', 'color'].includes(category) ? 'sm' : 'base';
+  const buttonSize = SMALL_BUTTON_CATEGORIES.includes(category)
+    ? BUTTON_SIZE.Sm
+    : BUTTON_SIZE.Base;
+
+  const style = { width: buttonSize === BUTTON_SIZE.Sm ? '100%' : 'initial' };
+  const isSizeCategory = category === 'size';
 
   return (
-    <div
-      mb="8 lg:2"
-      style={{ width: buttonSize === 'sm' ? '100%' : 'initial' }}
-    >
+    <div mb="8 lg:2" style={style}>
       <div font="medium">{label}</div>
       <div
         mt="1"
         grid="~ cols-3 gap-2"
         justify="between"
-        {...(category === 'size' && { grid: '~ cols-4 gap-2' })}
+        {...(isSizeCategory && { grid: '~ cols-4 gap-2' })}
       >
         {options.map(({ VALUE, LABEL }) => {
           const isPressed = getIsPressed(VALUE);
