5883e4a12f4c0ded56eb15e64c5f87d5949c7ede
Chin <greeting.to.chin@gmail.com>
2022-06-25 22:35:33 +0800

    feat(DistrictSelect): add district select

---
+++ app/components/common/DistrictSelect/index.tsx
@@ -0,0 +1,46 @@
+import { useState, useMemo, useEffect } from 'react';
+import SearchSelect from '~/components/common/SearchSelect';
+import { COUNTRIES, DISTRICTS_BY_COUNTRIES } from './constants';
+
+type Props = {
+  onFinish: ({
+    country,
+    district
+  }: {
+    country: string;
+    district: string;
+  }) => void;
+};
+
+export default function DistrictSelection({ onFinish }: Props) {
+  const [country, setCountry] = useState('');
+  const [district, setDistrict] = useState('');
+
+  const districtOptions = useMemo(
+    () => DISTRICTS_BY_COUNTRIES[country] || [],
+    [country]
+  );
+
+  const onDistrictSelect = (district: string) => {
+    setDistrict(district);
+    onFinish({ country, district });
+  };
+
+  useEffect(() => setDistrict(''), [country]);
+
+  return (
+    <fieldset flex="~" gap="2">
+      <SearchSelect
+        options={COUNTRIES}
+        placeholder="選擇城市"
+        onSelect={setCountry}
+      />
+      <SearchSelect
+        options={districtOptions}
+        placeholder="選擇地區"
+        disabled={!country}
+        onSelect={onDistrictSelect}
+      />
+    </fieldset>
+  );
+}
