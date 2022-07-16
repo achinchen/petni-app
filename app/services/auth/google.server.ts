bc4837b91f5b223d232be5e0c0d2abdfbfd88538
Chin <greeting.to.chin@gmail.com>
2022-07-16 21:55:44 +0800

    feat(services/auth): implement google oauth

---
+++ app/services/auth/google.server.ts
@@ -0,0 +1,36 @@
+import type { User } from '@prisma/client';
+import 'dotenv/config';
+import { Authenticator } from 'remix-auth';
+import { sessionStorage } from '~/services/session/index.server';
+import { db } from '~/utils/db/index.server';
+import { GoogleStrategy, PROVIDER_NAME } from './google.strategy.server';
+export { PROVIDER_NAME };
+
+export const authenticator = new Authenticator<User>(sessionStorage);
+
+authenticator.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_AUTH0_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_AUTH0_CLIENT_SECRET!,
+      callbackURL: 'http://localhost:3000/api/auth/google/callback'
+    },
+    async ({ profile }) => {
+      const { displayName: name, emails, photos } = profile;
+      const email = emails[0].value;
+      const imageUrl = photos[0].value;
+
+      const user = {
+        name,
+        email,
+        imageUrl
+      };
+
+      return db.user.upsert({
+        create: user,
+        update: user,
+        where: { email }
+      });
+    }
+  )
+);
