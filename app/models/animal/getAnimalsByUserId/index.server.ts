d2f941eb849dcdbd22f7ad6a2d71684cd1d03455
Chin <greeting.to.chin@gmail.com>
2022-07-17 20:50:21 +0800

    feat(models/getAnimalsByUserId): implement

---
+++ app/models/animal/getAnimalsByUserId/index.server.ts
@@ -0,0 +1,24 @@
+import type { User } from '@prisma/client';
+import type { SimpleAnimal } from '~/models/animal/type';
+import { db } from '~/utils/db/index.server';
+
+export type UserId = User['id'];
+
+export default async function getAnimalByIds(
+  id: UserId
+): Promise<SimpleAnimal[]> {
+  const animals = await db.animal.findMany({
+    where: {
+      userId: id
+    },
+    select: {
+      id: true,
+      gender: true,
+      family: true,
+      location: true,
+      imageUrl: true
+    }
+  });
+
+  return animals;
+}
