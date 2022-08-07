ef0a13945ec71975cc0759b434fc3e35eefa73e3
Chin <greeting.to.chin@gmail.com>
2022-08-07 16:09:48 +0800

    chore(constants): tidy up FAMILY_LABEL

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,3 +1,9 @@
+import { Family } from '@prisma/client';
 export { Family, Gender, Size } from '@prisma/client';
 
 export const APP_NAME = 'PetNi';
+
+export const FAMILY_LABEL = {
+  [Family.Cat]: '喵星人',
+  [Family.Dog]: '汪星人'
+};
