3a48f0b5470c4a9b2b75f6c56bf2628130530253
Chin <greeting.to.chin@gmail.com>
2022-08-18 10:09:38 +0800

    test(features/pairing/PairingCard): add basic test

---
+++ app/features/pairing/Cards/PairingCard/index.test.tsx
@@ -0,0 +1,75 @@
+import type { InitialState } from '~/features/pairing/context';
+import { render, screen } from '@testing-library/react';
+import { MockComponent } from 'spec/utils/getMockComponent';
+import { ANIMAL } from 'spec/__mock__/constants/animal';
+import PairingCard from '.';
+
+const testId = {
+  backCard: 'BackCard',
+  centerCard: 'CenterCard',
+  frontCard: 'FrontCard'
+};
+
+const mock: Pick<InitialState, 'currentCard'> = {
+  currentCard: ANIMAL
+};
+
+jest.mock('~/features/pairing/context', () => {
+  return {
+    __esModule: true,
+    usePairContext: () => ({
+      currentCard: mock.currentCard,
+      onNext: () => {}
+    })
+  };
+});
+
+jest.mock('./BackCard', () => MockComponent('BackCard'));
+jest.mock('./CenterCard', () => MockComponent('CenterCard'));
+jest.mock('./FrontCard', () => MockComponent('FrontCard'));
+
+describe('rendering: empty', () => {
+  beforeEach(() => {
+    mock.currentCard = undefined;
+    render(<PairingCard />);
+  });
+
+  test('render BackCard', () => {
+    expect(screen.getByTestId(testId.backCard).getAttribute('isempty')).toBe(
+      'true'
+    );
+  });
+
+  test('render CenterCard', () => {
+    expect(screen.getByTestId(testId.centerCard).getAttribute('isempty')).toBe(
+      'true'
+    );
+  });
+
+  test('not render FrontCard', () => {
+    expect(() => screen.getByTestId(testId.frontCard)).toThrow();
+  });
+});
+
+describe('rendering: not empty', () => {
+  beforeEach(() => {
+    mock.currentCard = ANIMAL;
+    render(<PairingCard />);
+  });
+
+  test('render BackCard with empty', () => {
+    expect(screen.getByTestId(testId.backCard).getAttribute('isempty')).toBe(
+      'false'
+    );
+  });
+
+  test('render CenterCard with empty', () => {
+    expect(screen.getByTestId(testId.centerCard).getAttribute('isempty')).toBe(
+      'false'
+    );
+  });
+
+  test('render FrontCard', () => {
+    expect(screen.getByTestId(testId.frontCard)).toBeDefined();
+  });
+});
