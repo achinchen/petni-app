8c798469abe791635239a2abc7264387c2842ef2
Chin <greeting.to.chin@gmail.com>
2022-08-21 08:09:33 +0800

    test(components/Layout/BackgroundSound): add basic test

--- app/components/common/Layout/BackgroundSound/constants.ts
+++ app/components/common/Layout/BackgroundSound/constants.ts
@@ -3,3 +3,5 @@ import dog from '~/assets/sounds/dog.mp3';
 import general from '~/assets/sounds/general.mp3';
 
 export const SOUND = { cat, dog, general };
+
+export const ALT = '音效';
--- app/components/common/Layout/BackgroundSound/index.tsx
+++ app/components/common/Layout/BackgroundSound/index.tsx
@@ -1,10 +1,16 @@
-import { SOUND } from './constants';
+import { SOUND, ALT } from './constants';
 
 export default function BackgroundSound() {
   return (
     <div display="none">
       {Object.entries(SOUND).map(([type, source]) => (
-        <audio key={type} id={`${type}-sound`} src={source} type="audio/mp3" />
+        <audio
+          key={type}
+          id={`${type}-sound`}
+          aria-label={ALT}
+          src={source}
+          type="audio/mp3"
+        />
       ))}
     </div>
   );
