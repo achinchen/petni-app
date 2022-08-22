f49ecf14ede5f76292530d8684a8aefb69107bee
Chin <greeting.to.chin@gmail.com>
2022-08-22 22:05:53 +0800

    refactor(pairing/ControlPanel/Filter): move onClick in FilterItem scope

--- app/features/pairing/ControlPanel/Filter/Item/index.tsx
+++ app/features/pairing/ControlPanel/Filter/Item/index.tsx
@@ -1,4 +1,5 @@
 import type { Category, Label, Options } from './type';
+import { useControlContext } from '~/features/pairing/ControlPanel/context';
 import Icon from '~/components/common/Icon';
 import OptionButton from './OptionButton';
 import { BUTTON_SIZE } from './constants';
@@ -7,25 +8,27 @@ type Props = {
   category: Category;
   label: Label;
   options: Options;
-  getIsPressed: (value: any) => boolean;
-  onClick: (value: any) => () => void;
 };
+
+type Value = Options[number]['VALUE'];
 
 const SMALL_BUTTON_CATEGORIES = ['size', 'color'];
 
-export default function Filter({
-  category,
-  label,
-  options,
-  getIsPressed,
-  onClick
-}: Props) {
+export default function Filter({ category, label, options }: Props) {
+  const { filter, dispatchFilter } = useControlContext();
+  const currentFilter = filter[category];
+
   const buttonSize = SMALL_BUTTON_CATEGORIES.includes(category)
     ? BUTTON_SIZE.Sm
     : BUTTON_SIZE.Base;
 
   const style = { width: buttonSize === BUTTON_SIZE.Sm ? '100%' : 'initial' };
   const isSizeCategory = category === 'size';
+
+  const getIsPressed = (value: Value) => currentFilter === value;
+
+  const onClick = (value: Value) => () =>
+    dispatchFilter({ type: category, value });
 
   return (
     <div mb="8 lg:4" style={style}>
