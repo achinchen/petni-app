275e2b41adaf2aa42ab5befd92f3409becb33c7d
Chin <greeting.to.chin@gmail.com>
2022-08-23 08:29:14 +0800

    chore(spec/setup/remix-node): handle installGlobals in test env

---
+++ spec/setup/remix-node.ts
@@ -0,0 +1,16 @@
+import type { DeepMockProxy } from 'jest-mock-extended';
+import { mockReset } from 'jest-mock-extended';
+import { installGlobals } from '@remix-run/node';
+
+jest.mock('@remix-run/node', () => ({
+  __esModule: true,
+  installGlobals: jest.fn()
+}));
+
+beforeEach(() => {
+  mockReset(RemixNodeMock);
+});
+
+export const RemixNodeMock = installGlobals as unknown as DeepMockProxy<
+  typeof installGlobals
+>;
