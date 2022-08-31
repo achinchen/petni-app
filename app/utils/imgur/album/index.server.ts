6db1ee979e7337ce39d691d4f70a6960feef7be7
Chin <greeting.to.chin@gmail.com>
2022-08-31 22:34:23 +0800

    test(utils/imgur/album): add basic test

--- app/utils/imgur/album/index.server.ts
+++ app/utils/imgur/album/index.server.ts
@@ -1,13 +1,12 @@
-import fetch, { FormData } from 'node-fetch';
 import { ENDPOINT, HEADERS } from '~/utils/imgur/constants';
+
+export const TITLE = 'Petni App Dev';
+export const DESCRIPTION = 'This album provides some animals could be adopted!';
 
 export async function createAlbum() {
   const formData = new FormData();
-  formData.set('title', 'Petni App Dev');
-  formData.set(
-    'description',
-    'This album provides some animals could be adopted!'
-  );
+  formData.set('title', TITLE);
+  formData.set('description', DESCRIPTION);
 
   const response = await fetch(ENDPOINT.ALBUM.CREATE, {
     method: 'POST',
