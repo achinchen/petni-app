deaf822a472e6c9c95855a9edc29416ee8a0a3c5
Chin <greeting.to.chin@gmail.com>
2022-09-04 20:57:32 +0800

    chore(utils/seo): add getMetaBaseByAnimal

---
+++ app/utils/seo/getMetaBaseByAnimal.ts
@@ -0,0 +1,24 @@
+import type { Animal } from '@prisma/client';
+import { DEFAULT_META } from '~/constants/meta';
+import { APP_NAME } from '~/constants';
+
+type Parameters = {
+  animal?: Animal;
+  prefix?: {
+    title?: string;
+    description?: string;
+  };
+};
+
+export default function getMetaByAnimals({ animal, prefix = {} }: Parameters): {
+  title: string;
+  description: string;
+} {
+  if (!animal) return DEFAULT_META;
+  const { id, location } = animal;
+  const { title = '', description = '' } = prefix;
+  return {
+    title: `${title}No.${id} ｜ ${APP_NAME} - 陪你找家`,
+    description: `${description}No.${id} - 正在 ${location} 等家`
+  };
+}
