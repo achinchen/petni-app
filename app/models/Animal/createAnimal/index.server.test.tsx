6b696769987cb0f013fdde1852f7553d953e53d6
Chin <greeting.to.chin@gmail.com>
2022-09-06 08:11:48 +0800

    test(models/Animal/createAnimal): add basic test

---
+++ app/models/Animal/createAnimal/index.server.test.tsx
@@ -0,0 +1,55 @@
+import type { User, Animal } from '@prisma/client';
+import type { EditingAnimal } from '~/models/Animal/type';
+import createAnimal from './index.server';
+import { db } from '~/utils/db/index.server';
+import { ANIMAL } from 'spec/mock/constants/animal';
+import { USER } from 'spec/mock/constants/user';
+
+let user: User;
+beforeAll(async () => {
+  const { id, ...payload } = USER;
+  user = await db.user.create({
+    data: payload
+  });
+});
+
+afterAll(async () => {
+  const deleteUsers = db.user.deleteMany();
+  const deleteAnimals = db.animal.deleteMany();
+  await db.$transaction([deleteUsers, deleteAnimals]);
+  await db.$disconnect();
+});
+
+describe('createAnimal', () => {
+  let animal: Animal;
+  beforeAll(async () => {
+    animal = await createAnimal(ANIMAL, user);
+  });
+
+  it('animal.address is same with animal.location', () => {
+    expect(animal.address).toBe(animal.location);
+  });
+
+  it('return animal', () => {
+    expect(animal).toBeTruthy();
+  });
+
+  it('animal be created', async () => {
+    const createdAnimal = await db.animal.findUnique({
+      where: { id: animal.id }
+    });
+    expect(createdAnimal).toBeTruthy();
+  });
+});
+
+// export default async (animal: EditingAnimal, user: User): Promise<Animal> => {
+//   const record = await db.animal.create({
+//     data: {
+//       ...animal,
+//       address: animal.location,
+//       userId: user.id
+//     }
+//   });
+
+//   return record;
+// };
