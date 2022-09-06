0072441b2b2074384123d8f2020c54249162d2c1
Chin <greeting.to.chin@gmail.com>
2022-09-06 21:53:23 +0800

    test(models/Animal/getAnimalsByOptions/getNearCitiesByCity): add basic test

--- app/models/Animal/getAnimalsByOptions/getNearCitiesByCity.ts
+++ app/models/Animal/getAnimalsByOptions/getNearCitiesByCity.ts
@@ -7,8 +7,10 @@ import {
 export default function getNearCitiesByCity(city: string) {
   if (OUTLYING_ISLAND_CITIES.includes(city)) return [city];
 
-  const index = CITIES.indexOf(city);
+  let index = CITIES.indexOf(city);
   const { START, END } = DEFAULT_CITY_RANGE;
+
+  if (!index) index += 1;
 
   const startIndex = index === -1 ? START : index - 1;
   const endIndex = index === -1 ? END : index + 1;
