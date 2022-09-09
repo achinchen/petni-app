60517e9c002e339b2bb3739a793f85bba3322270
Chin <greeting.to.chin@gmail.com>
2022-09-09 13:26:23 +0800

    chore(types/react): handle SVGAttributes with Attributify props

--- types/react.d.ts
+++ types/react.d.ts
@@ -1,8 +1,10 @@
-import { AttributifyOptions } from '@unocss/preset-attributify';
+import type { AttributifyOptions } from '@unocss/preset-attributify';
 
 declare module 'react' {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface HTMLAttributes<T> extends AttributifyOptions {}
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface SVGAttributes<T> extends AttributifyOptions {}
 
   interface CSSProperties {
     [key: `--${string}`]: string | number;
