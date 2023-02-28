e9aa92c177d08aefbbe6d171c3510f5202a98597
Chin <greeting.to.chin@gmail.com>
2023-02-28 18:49:57 +0800

    refactor(jest.config): support server folder

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
   moduleNameMapper: {
     '~/(.*)$': '<rootDir>/app/$1',
-    'spec/(.*)$': '<rootDir>/spec/$1',
+    'spec/(.*)$': '<rootDir>/spec/$1'
   },
   clearMocks: true,
   preset: 'ts-jest',
   transform: {
-    '^.+\\.tsx?$': "ts-jest",
+    '^.+\\.tsx?$': 'ts-jest',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-    '<rootDir>/spec/utils/fileTransformer.ts',
+      '<rootDir>/spec/utils/fileTransformer.ts'
   }
-}
+};
 
 module.exports = {
   projects: [
@@ -19,14 +19,14 @@ module.exports = {
       displayName: 'client',
       testEnvironment: 'jsdom',
       testPathIgnorePatterns: ['server.test.tsx?'],
-      setupFilesAfterEnv: ['<rootDir>/spec/setup/client.ts'],
+      setupFilesAfterEnv: ['<rootDir>/spec/setup/client.ts']
     },
     {
       ...CONFIG,
       displayName: 'server',
       testEnvironment: 'node',
-      testRegex: [/server.test.tsx?/],
-      setupFilesAfterEnv: ['<rootDir>/spec/setup/server.ts'],
-    },
+      testRegex: [/server.test.tsx?/, '/server/**/*.test.ts?'],
+      setupFilesAfterEnv: ['<rootDir>/spec/setup/server.ts']
+    }
   ]
 };
