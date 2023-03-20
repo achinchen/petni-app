b69bcffdb0aa181392b1739b4668e74c5f89d4d1
Chin <greeting.to.chin@gmail.com>
2023-03-20 21:46:22 +0800

    feat(usecases/animal): implement getAnimalInfo

--- server/usecases/animal.ts
+++ server/usecases/animal.ts
@@ -1,10 +1,19 @@
 import type { EditingAnimal } from '~/models/Animal/type';
 import type { Animal } from 'server/entities/animal';
 import type { AnimalRepository } from 'server/gateways/animal';
+import type { AnimalFollowRepository } from 'server/gateways/animal-follow';
+import type { AnimalFollow } from 'server/entities/animal-follow';
 import type { User } from 'server/entities/user';
 
+export type AnimalInfo = Animal & {
+  count: AnimalFollow['count'];
+  editable: boolean;
+};
 export class AnimalUseCase {
-  constructor(private readonly animalRepository: AnimalRepository) {}
+  constructor(
+    private readonly animalRepository: AnimalRepository,
+    private readonly animalFollowRepository?: AnimalFollowRepository
+  ) {}
 
   async getCreatedAnimals(userId: User['id']): Promise<Animal[]> {
     const animals = await this.animalRepository.getManyByUserId!(userId);
@@ -19,9 +28,19 @@ export class AnimalUseCase {
   async getAnimalInfo(
     animalId: Animal['id'],
     userId?: User['id']
-  ): Promise<Animal | null> {
+  ): Promise<AnimalInfo | null> {
     const animal = await this.animalRepository.getOneById(animalId, userId);
-    return animal;
+    const animalFollow = await this.animalFollowRepository?.getOneByAnimalId(
+      animalId
+    );
+    const count = animalFollow?.count || 0;
+    const editable = userId ? animal?.userId === userId : false;
+
+    return {
+      ...animal,
+      count,
+      editable
+    } as AnimalInfo;
   }
 
   async createAnimal(payload: EditingAnimal): Promise<Animal> {
