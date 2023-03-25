c5438db95f6ee24b6eb1f5cce144ddb9e2bf4dac
Chin <greeting.to.chin@gmail.com>
2023-03-25 22:50:18 +0800

    test(server/adapters/presenter): add test case

---
+++ server/adapters/animal/presenter.test.ts
@@ -0,0 +1,26 @@
+import { AnimalPresenter } from './index.presenter';
+import { ANIMAL } from 'spec/mock/constants/animal';
+
+describe('AnimalPresenter', () => {
+  const animalFollowPresenter = new AnimalPresenter();
+
+  const testCases = [
+    {
+      expected: [400],
+      method: animalFollowPresenter.invalidInput
+    },
+    {
+      expected: [405],
+      method: animalFollowPresenter.invalidMethod
+    },
+    { expected: [500], method: animalFollowPresenter.failed },
+    { expected: [200, ANIMAL], method: animalFollowPresenter.success }
+  ];
+
+  it.concurrent.each(testCases)(
+    'return $expected when $method.name',
+    ({ expected, method }) => {
+      expect(method(ANIMAL)).toEqual(expected);
+    }
+  );
+});
