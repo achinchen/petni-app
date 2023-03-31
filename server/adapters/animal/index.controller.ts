69cb03fadf180f766ca2eb7c11065e356a6c25e6
Chin <greeting.to.chin@gmail.com>
2023-03-31 22:45:01 +0800

    feat(adapters/animal): implement updateAnimal into controller

--- server/adapters/animal/index.controller.ts
+++ server/adapters/animal/index.controller.ts
@@ -22,4 +22,20 @@ export class AnimalController {
       return this.animalPresenter.failed();
     }
   }
-}
+
+  async updateAnimal(
+    payload: Partial<Animal> & Pick<Animal, 'id'>,
+    userId: User['id']
+  ): Promise<Payload> {
+    const { id, ...rest } = payload;
+    if (!userId) return this.animalPresenter.forbidden();
+    if (!Object.entries(rest).flat().filter(Boolean).length)
+      return this.animalPresenter.invalidInput();
+    try {
+      const result = await this.animalUseCase.updateAnimal(payload, userId);
+      if (!result) return this.animalPresenter.failed();
+      return this.animalPresenter.success(result);
+    } catch {
+      return this.animalPresenter.failed();
+    }
+  }
