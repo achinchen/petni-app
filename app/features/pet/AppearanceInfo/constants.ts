6820f8ff6b22ae72175849e900c2a2741781d6db
Chin <greeting.to.chin@gmail.com>
2023-08-14 15:58:15 +0800

    fix: appearance info

--- app/features/pet/AppearanceInfo/constants.ts
+++ app/features/pet/AppearanceInfo/constants.ts
@@ -3,14 +3,14 @@ import { DOG_COLOR_LABEL } from '~/constants/dogs';
 import { CAT_COLOR_LABEL } from '~/constants/cats';
 
 export const FAMILY_COLOR_LABEL = {
-  [Family.Dog]: DOG_COLOR_LABEL,
-  [Family.Cat]: CAT_COLOR_LABEL
+  [Family[Family.Dog]]: DOG_COLOR_LABEL,
+  [Family[Family.Cat]]: CAT_COLOR_LABEL
 } as const;
 
 export const ADAPT_ME_LABEL = '求包養';
 
 export const GENDER_LABEL = {
-  [Gender.Female]: '女生',
-  [Gender.Male]: '男生',
-  [Gender.Null]: '未知'
+  [Gender[Gender.Female]]: '女生',
+  [Gender[Gender.Male]]: '男生',
+  [Gender[Gender.Null]]: '未知'
 };
--- app/features/pet/AppearanceInfo/index.tsx
+++ app/features/pet/AppearanceInfo/index.tsx
@@ -4,7 +4,6 @@ import { getIconByGenderAndFamily } from '~/utils/icon';
 import { ColorPlatte, Note } from '~/components/common/Icon';
 import Card from '~/components/common/Card';
 import LabelIcon from './LabelIcon';
-import { getColorLabelByFamilyAndColor } from './utils';
 import { GENDER_LABEL, ADAPT_ME_LABEL } from './constants';
 
 export default function AppearanceInfo() {
@@ -12,18 +11,13 @@ export default function AppearanceInfo() {
   const { gender, family, color } = pet;
   const genderIcon = getIconByGenderAndFamily({ gender, family });
   const genderLabel = GENDER_LABEL[gender];
-  const colorLabel = getColorLabelByFamilyAndColor({ color, family });
 
   return (
     <Card mb="3" py="2 md:6" font="medium">
       <div m="auto" flex="~" justify="between" max-w="at-sm:86">
         <LabelIcon color="status-active" icon={Note} label={ADAPT_ME_LABEL} />
         <LabelIcon {...genderIcon} label={genderLabel} />
-        <LabelIcon
-          color="status-active"
-          icon={ColorPlatte}
-          label={colorLabel}
-        />
+        <LabelIcon color="status-active" icon={ColorPlatte} label={color} />
       </div>
     </Card>
   );
